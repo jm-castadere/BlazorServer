c9da721b380a4d681fa62f3a38a1b8e6d16567f7
jean-michel Castadere <castadere@online.de>
2021-06-29 10:06:37 +0200

    add service cal API

--- BlazorService/HtppWeatherService.cs
+++ BlazorService/HtppWeatherService.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BlazorModel;
+using System;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using BlazorModel;
 
 namespace BlazorService
 {
+    /// <summary>
+    /// Service to call API
+    /// </summary>
     public class HtppWeatherService: IWeatherForecastService
     {
         private readonly HttpClient _client;
 
         /// <summary>
-        /// Constructor
+        /// Constructor set htpp api
         /// </summary>
         /// <param name="client"></param>
         public HtppWeatherService(HttpClient client)
@@ -25,14 +25,14 @@ namespace BlazorService
 
 
         /// <summary>
-        /// Get all value
+        /// Service Get all value
         /// </summary>
         /// <param name="startDate"></param>
         /// <returns></returns>
         public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            //call 
-            var response = await _client.GetAsync("weatherForecast");
+            //call API-> WeatherForecastController
+            var response = await _client.GetAsync("WeatherForecast");
 
             if (response.IsSuccessStatusCode)
             {
